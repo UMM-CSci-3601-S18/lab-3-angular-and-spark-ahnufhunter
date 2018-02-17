84df72e4b19bc7ad21a0e36bd05a577eb70cf2d8
jubairmr9 <hassa357@morris.umn.edu>
2018-02-17 16:09:52 -0600

    created a todo-list.service.spec.ts file

---
+++ client/src/app/todos/todo-list.service.spec.ts
@@ -0,0 +1,90 @@
+import {HttpClientTestingModule, HttpTestingController} from '@angular/common/http/testing';
+import {TestBed} from '@angular/core/testing';
+import {HttpClient} from '@angular/common/http';
+
+import {Todo} from './todo';
+import {TodoListService} from './todo-list.service';
+
+describe('Todo list service: ', () => {
+  // A small collection of test todos
+  const testTodos: Todo[] = [
+    {
+      id: '58895985170cd3b13cfafeec',
+      owner: 'Workman',
+      status: false,
+      body: 'Ea adipisicing amet exercitation excepteur ea reprehenderit nulla reprehenderit eiusmod commodo occaecat. Velit dolor enim Lorem et irure cupidatat ex pariatur non.',
+      category: 'homework'
+    },
+    {
+      id: '5889598502be34bcf1e1a333',
+      owner: 'Barry',
+      status: false,
+      body: 'Consectetur aute enim ullamco fugiat est. Eu dolore fugiat mollit sit ut laborum labore est.',
+      category: 'homework'
+    },
+    {
+      id: '58895985921f087bb21de23e',
+      owner: 'Dawn',
+      status: true,
+      body: 'Sunt exercitation culpa non ad exercitation quis excepteur cupidatat occaecat eiusmod. Aliquip aute anim fugiat elit ad ad cillum dolore qui dolore do commodo culpa velit.',
+      category: 'groceries'
+    }
+  ];
+  let todoListService: TodoListService;
+  // These are used to mock the HTTP requests so that we (a) don't have to
+  // have the server running and (b) we can check exactly which HTTP
+  // requests were made to ensure that we're making the correct requests.
+  let httpClient: HttpClient;
+  let httpTestingController: HttpTestingController;
+
+  beforeEach(() => {
+    // Set up the mock handling of the HTTP requests
+    TestBed.configureTestingModule({
+      imports: [HttpClientTestingModule]
+    });
+    httpClient = TestBed.get(HttpClient);
+    httpTestingController = TestBed.get(HttpTestingController);
+    // Construct an instance of the service with the mock
+    // HTTP client.
+    todoListService = new TodoListService(httpClient);
+  });
+
+  afterEach(() => {
+    // After every test, assert that there are no more pending requests.
+    httpTestingController.verify();
+  });
+
+  it('getTodos() calls api/todos', () => {
+    // Assert that the todos we get from this call to getTodos()
+    // should be our set of test todos. Because we're subscribing
+    // to the result of getTodos(), this won't actually get
+    // checked until the mocked HTTP request "returns" a response.
+    // This happens when we call req.flush(testTodos) a few lines
+    // down.
+    todoListService.getTodos().subscribe(
+      todos => expect(todos).toBe(testTodos)
+    );
+
+    // Specify that (exactly) one request will be made to the specified URL.
+    const req = httpTestingController.expectOne(todoListService.todoUrl);
+    // Check that the request made to that URL was a GET request.
+    expect(req.request.method).toEqual('GET');
+    // Specify the content of the response to that request. This
+    // triggers the subscribe above, which leads to that check
+    // actually being performed.
+    req.flush(testTodos);
+  });
+
+  it('getTodoById() calls api/todos/id', () => {
+    const targetTodo: Todo = testTodos[1];
+    const targetId: string = targetTodo.id;
+    todoListService.getTodoById(targetId).subscribe(
+      todo => expect(todo).toBe(targetTodo)
+    );
+
+    const expectedUrl: string = todoListService.todoUrl + '/' + targetId;
+    const req = httpTestingController.expectOne(expectedUrl);
+    expect(req.request.method).toEqual('GET');
+    req.flush(targetTodo);
+  });
+});
